04e11a191c2777383886f8a43ed9e4766e28cb92
Naufal Yukafi <naufalyukafi10@gmail.com>
2020-09-07 11:23:19 +0700

    implement ui login page

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,9 +1,22 @@
 import React from "react";
-
+import { LoginBg } from "../../assets";
+import { Input, Button, Gap, Link } from "../../components";
 const Login = () => {
   return (
-    <div>
-      <h1>Login Pages</h1>
+    <div className="main-page">
+      <div className="left">
+        <img src={LoginBg} className="bg-image" alt="img-bg" />
+      </div>
+      <div className="right">
+        <p className="title">Login</p>
+        <Input label="Email" placeholder="Email" />
+        <Gap height={18} />
+        <Input label="Password" placeholder="Password" type="password" />
+        <Gap height={20} />
+        <Button title="Login" />
+        <Gap height={50} />
+        <Link title="Belum punya akun, silahkan daftar" />
+      </div>
     </div>
   );
 };
